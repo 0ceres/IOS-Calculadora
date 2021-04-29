f38b71fd5857f6ce1825989f535c6b462c553f8d
0ceres <oceres@hotmail.com>
2021-04-29 11:13:24 -0400

    ahora si un commit de la calculadora con layout
    calculadora con layout y auto aling

---
+++ IOS-Calculadora/Controller/Home/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  IOS-Calculadora
+//
+//  Created by Roger Orlando Ceres Salinas on 28/4/21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    //MARK: - Initialization
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Life cicle
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+}
